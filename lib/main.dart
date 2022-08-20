f66d43bbafb8a5369998d0bd17641c47d9b78625
baxriddin2006 <baxriddinoff000@gmail.com>
2022-08-20 16:42:41 +0500

    20.08.2022.16:42

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:taxiapp/pages/splash_screen.dart';
 import 'package:taxiapp/pages/start_page.dart';
@@ -8,6 +9,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
@@ -27,13 +29,50 @@ class MyApp extends StatelessWidget {
             child: Column(
               children: [
                 Image.asset('assets/images/Group 4.png'),
-                Text("M Y  T A X I",style: TextStyle(fontSize: 60,fontWeight: FontWeight.bold,color: Colors.yellow),)
+                SizedBox(height: 10,),
+                Container(
+                  width: 245,
+                  height: 109,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children:[
+                      Text(
+                        "Taxi",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Color(0xfffec02a),
+                          fontSize: 90,
+                          fontFamily: "Inter",
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      Text(
+                        "o",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Color(0xfffec02a),
+                          fontSize: 90,
+                          fontFamily: "Inter",
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                CupertinoActivityIndicator(
+                  radius: 30,
+                  color: Colors.deepOrange,
+                )
               ],
             )
           ),
           nextScreen: SplashScreen(),
-          splashTransition: SplashTransition.decoratedBoxTransition,
+          splashTransition: SplashTransition.fadeTransition,
           splashIconSize: 600,
+
         ),
       )
     );
