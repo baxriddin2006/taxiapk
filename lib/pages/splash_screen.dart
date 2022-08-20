5881fb352595830e89e30b9dc5db2b44bc88c0ba
baxriddin2006 <baxriddinoff000@gmail.com>
2022-08-20 18:10:03 +0500

    20.08.2022.17.02

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:slide_to_act/slide_to_act.dart';
+import 'package:slider_button/slider_button.dart';
 import 'package:taxiapp/pages/pages.dart';
 
 class SplashScreen extends StatefulWidget {
-  static final String id = "";
+  static const String id = "";
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
@@ -15,90 +16,94 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     // ignore: prefer_const_constructors
     return Scaffold(
-      body: Container(
-        child: Stack(
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              child: Image.asset(
-                'assets/images/image 2.png',
-                fit: BoxFit.cover,
+      body: Stack(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              'assets/images/image 2.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          const Positioned(
+            top: 50,
+            left: 30,
+            child: Text(
+              "Xush Kelibsiz!",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 45,
+                fontFamily: "Inter",
+                fontWeight: FontWeight.w700,
               ),
             ),
-            Positioned(
-              top: 50,
-              left: 30,
-              child: Text(
-                "Xush Kelibsiz!",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 45,
-                  fontFamily: "Inter",
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-            ),
-            Positioned(
-              top: 100,
-              left: 30,
-              child: Row(
-                children: [
-                  Text(
-                    "Taxi",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Color(0xfffec02a),
-                      fontSize: 90,
-                      fontFamily: "Inter",
-                      fontWeight: FontWeight.w700,
-                    ),
+          ),
+          Positioned(
+            top: 100,
+            left: 30,
+            child: Row(
+              children: const [
+                Text(
+                  "Taxi",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Color(0xfffec02a),
+                    fontSize: 90,
+                    fontFamily: "Inter",
+                    fontWeight: FontWeight.w700,
                   ),
-                  Text(
-                    "o",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Color(0xfffec02a),
-                      fontSize: 90,
-                      fontFamily: "Inter",
-                      fontWeight: FontWeight.w700,
-                    ),
+                ),
+                Text(
+                  "o",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Color(0xfffec02a),
+                    fontSize: 90,
+                    fontFamily: "Inter",
+                    fontWeight: FontWeight.w700,
                   ),
-                ],
+                ),
+              ],
+            ),
+          ),
+          const Positioned(
+            top: 200,
+            left: 30,
+            child: Text(
+              "Asrning eng yaxshi taksi bron qilish ilovasida\nkuningizni ajoyib o'tkazing!",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontFamily: "Jost",
+                fontWeight: FontWeight.w500,
               ),
             ),
-            const Positioned(
-              top: 200,
-              left: 30,
-              child: Text(
-                "Asrning eng yaxshi taksi bron qilish ilovasida\nkuningizni ajoyib o'tkazing!",
+          ), //Slider Buttn
+          Positioned(
+            top: 700,
+            child: Center(child: SliderButton(
+              backgroundColor: Colors.white.withOpacity(0.7),
+              width: 392,
+              action: () {
+
+                Navigator.pushReplacement(
+                    context, MaterialPageRoute(builder: (context) => const Pages()));
+              },
+                alignLabel: Alignment.center,
+              label: Text(
+                "Boshlash",
                 style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontFamily: "Jost",
-                  fontWeight: FontWeight.w500,
-                ),
+                    color: Color(0xff4a4a4a), fontWeight: FontWeight.w500, fontSize: 22,),
               ),
-            ),
-            //Slider pages
-            Positioned(
-              top: 700,
-              child: SlideAction(
-                text: "Boshlash",
-                innerColor: Colors.white,
-                outerColor: Colors.white.withOpacity(0.7),
-                sliderButtonIcon: Icon(Icons.local_taxi_sharp),
-                onSubmit: () => Navigator.push(
-                    context, MaterialPageRoute(builder: (context) => Pages())),
-                submittedIcon: Icon(
-                  Icons.local_taxi,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ],
-        ),
+              icon:Image.asset('assets/images/taxi car.png')
+                
+
+            ))
+          ),
+
+        ],
       ),
     );
   }
