8fb07b3cdd4a7d5773d5c9b47600180381d8f7fd
baxriddin2006 <baxriddinoff000@gmail.com>
2022-08-20 18:26:49 +0500

    20.08.2022.18:21

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -91,6 +91,7 @@ class _SplashScreenState extends State<SplashScreen> {
                 Navigator.pushReplacement(
                     context, MaterialPageRoute(builder: (context) => const Pages()));
               },
+                //
                 alignLabel: Alignment.center,
               label: Text(
 
